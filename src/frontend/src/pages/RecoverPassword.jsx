f3ab8ac9c5d589d144046e2ed993ca5597173819
GabrielHMA01 <gabriel.martins@sou.inteli.edu.br>
2025-05-07 13:52:02 -0300

    fix: adjust margin botton

--- src/frontend/src/pages/RecoverPassword.jsx
+++ src/frontend/src/pages/RecoverPassword.jsx
@@ -29,7 +29,7 @@ const RecoverPassword = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="w-full mb-10 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Digite seu email"
             />
           </div>
