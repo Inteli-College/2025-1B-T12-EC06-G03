c5636106989c9fad13e2a390fa0d75a0aa756dfc
carol-pascarelli <carolina.santos@sou.inteli.edu.br>
2025-05-14 11:22:45 -0300

    feat:adiciona estilizacao

--- src/frontend/src/pages/UploadImagens.jsx
+++ src/frontend/src/pages/UploadImagens.jsx
@@ -1,11 +1,25 @@
-import React, {useState} from 'react';
+import React from 'react';
+import ImagensCarregadas from '../components/ImagensCarregadas.jsx';
 
 const UploadImagens = () => {
     return (
-        <div className="min-h-screen bg-slate-100 p-8" >
-            <h1 className="text-3xl font-bold mb-6">Upload de Imagem</h1>
+        <div className='min-h-screen bg-slate-100 p-8' >
+            <h1 className='text-3xl font-bold mb-6 text-dark-blue'>Upload de Imagem</h1>
+            <div className='bg-gray-light h-72 flex items-center justify-center rounded-md mb-10'>
+                <button className='bg-dark-blue text-gray-light px-6 py-2 rounded-xl shadow-md'>
+                    Carregar Imagem
+                </button>
+            </div>
+
+            <h2 className='text-3xl font-bold mb-6 text-dark-blue'>Imagens Carregadas</h2>
+
+            <div className='grid grid-cols-3 gap-x-8 gap-y-8'>
+                {Array.from({ length: 6 }).map((_, index) => (
+                    <ImagensCarregadas key={index} name={`Imagem ${index + 1}`} />
+                ))}
+            </div>
         </div>
     );
-}
+};
 
 export default UploadImagens;
