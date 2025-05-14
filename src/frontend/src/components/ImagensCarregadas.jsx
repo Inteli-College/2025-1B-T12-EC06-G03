139abdb29652f6b37fbbd54e9b6fe3ec01f2c553
carol-pascarelli <carolina.santos@sou.inteli.edu.br>
2025-05-14 11:06:13 -0300

    feat/ adiciona componente imagens

---
+++ src/frontend/src/components/ImagensCarregadas.jsx
@@ -0,0 +1,11 @@
+const ImagensCarregadas = ({ name }) => (
+  <div className="bg-blue-dark rounded-md shadow-md ">
+    <div className="bg-white m-5 aspect-[4/3] rounded-md flex items-center justify-center p-14">
+      <span className="text-gray-light text-xl">Preview da Imagem</span>
+    </div>
+    <div className="flex justify-between items-center">
+    </div>
+  </div>
+)
+
+export default ImagensCarregadas;
