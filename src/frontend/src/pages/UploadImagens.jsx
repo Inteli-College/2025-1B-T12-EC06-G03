c9ea80a91241e38e8e7ee4fc5fbe950f82bdf0bb
carol-pascarelli <carolina.santos@sou.inteli.edu.br>
2025-05-14 10:39:01 -0300

    feat/adiciona pagina uploads

--- src/frontend/src/pages/UploadImagens.jsx
+++ src/frontend/src/pages/UploadImagens.jsx
@@ -1,7 +1,9 @@
+import React, {useState} from 'react';
+
 const UploadImagens = () => {
     return (
         <div className="min-h-screen bg-slate-100 p-8" >
-            <h1 className="text-2xl font-bold mb-6"></h1>
+            <h1 className="text-3xl font-bold mb-6">Upload de Imagem</h1>
         </div>
     );
 }
