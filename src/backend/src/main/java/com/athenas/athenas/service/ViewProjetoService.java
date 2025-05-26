5c4c6503118eddfe8ceceb88651630471068b200
carol-pascarelli <carolina.santos@sou.inteli.edu.br>
2025-05-26 00:18:40 -0300

    feat:adiciona limite de caracteres log

--- src/backend/src/main/java/com/athenas/athenas/service/ViewProjetoService.java
+++ src/backend/src/main/java/com/athenas/athenas/service/ViewProjetoService.java
@@ -148,6 +148,12 @@ public class ViewProjetoService {
     }
 
     private void createLog(String descricao, Projeto projeto){
+            final int MAX_LOG_LENGTH = 255; // Limite de caracteres para VARCHAR(255)
+
+            if (descricao.length() > MAX_LOG_LENGTH) {
+              descricao = descricao.substring(0, MAX_LOG_LENGTH - 3) + "...";
+             }
+             
         LogAlteracao log = new LogAlteracao();
         log.setProjeto(projeto);
         log.setDescricao(descricao);
