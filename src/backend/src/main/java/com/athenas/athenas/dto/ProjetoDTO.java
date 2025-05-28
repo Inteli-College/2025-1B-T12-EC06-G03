e5c3e484883827c55f4b9d6c7004fc1566b19473
HeitorCand <heitor.candido@sou.inteli.edu.br>
2025-05-28 14:23:54 -0300

    feat: folder name

--- src/backend/src/main/java/com/athenas/athenas/dto/ProjetoDTO.java
+++ src/backend/src/main/java/com/athenas/athenas/dto/ProjetoDTO.java
@@ -1,6 +1,10 @@
 package com.athenas.athenas.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
